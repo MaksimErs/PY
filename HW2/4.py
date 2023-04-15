3921db4cfeef94d09588cb6dff56860d65ead54a
MaksimErs <eroken@mail.ru>
2023-04-15 18:15:51 +0300

    дз2

---
+++ HW2/4.py
@@ -0,0 +1,7 @@
+p = [1, 0, 0, 1, 1]
+f = [i for i in p if i == 0 ]
+print(f)
+v = [i for i in p if i == 1]
+print(v)
+if len(f) < len(v): print(len(f), 'монет перевернем')
+else: print(len(v), 'монет перевернем')
